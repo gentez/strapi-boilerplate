52d19dcffb9a00c21ab05a23e8fb26e5f48bb3e7
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-08-16 22:45:32 +0500

    first commit

---
+++ config/env/production/database.js
@@ -0,0 +1,22 @@
+// path: ./config/env/production/database.js
+
+const { parse } = require("pg-connection-string");
+
+module.exports = ({ env }) => {
+  const { host, port, database, user, password } = parse("host=localhost port=5432 dbname=postgres user=postgres password=xxxxxxx sslmode=prefer connect_timeout=10");
+  
+  return {
+    connection: {
+      client: 'postgres',
+      connection: {
+        host,
+        port,
+        database,
+        user,
+        password,
+        ssl: { rejectUnauthorized: false },
+      },
+      debug: false,
+    },
+  }
+};
