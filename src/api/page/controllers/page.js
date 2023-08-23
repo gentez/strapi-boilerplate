60c6a90411386b56ea3adf3d35d8bc0279ba28a7
Muhammad Fahad <106611736+MFahad11@users.noreply.github.com>
2023-08-23 12:53:55 +0500

    Structure updated

--- src/api/page/controllers/page.js
+++ src/api/page/controllers/page.js
@@ -10,17 +10,52 @@ module.exports = createCoreController('api::page.page',({ strapi }) =>  ({
     // Method 1: Creating an entirely custom action
     async findBySlug(ctx) {
       const { slug } = ctx.params;
-    //   console.log(slug)
-      // const query = `?filters[slug]=${slug}`;
-    //   console.log(query)
-      const page = await strapi.entityService.findMany('api::page.page',{
-        filters: {
-          slug:`${slug}`
-        },
-      });
-    //   console.log(page)
-      if (page) {
-        ctx.body = page;
+    const page = await strapi.entityService.findMany('api::page.page', {
+      filters: {
+        slug: slug,
+      },
+      populate: '*',
+    });
+    
+    // Process the populated response to remove unwanted fields
+    const sanitizedPage = page.map(pageItem => {
+      const sanitizedPageItem = { ...pageItem };
+    
+      // Remove the 'created_at' field from populated components
+      if (sanitizedPageItem.sidebar) {
+        sanitizedPageItem.sidebar = sanitizeComponent(sanitizedPageItem.sidebar);
+      }
+      if (sanitizedPageItem.navbar) {
+        sanitizedPageItem.navbar = sanitizeComponent(sanitizedPageItem.navbar);
+      }
+      if (sanitizedPageItem.footer) {
+        sanitizedPageItem.footer = sanitizeComponent(sanitizedPageItem.footer);
+      }
+      if (sanitizedPageItem.createdBy) {
+        delete sanitizedPageItem.createdBy;
+      }
+      if (sanitizedPageItem.updatedBy) {
+        delete sanitizedPageItem.updatedBy;
+      }
+    
+      return sanitizedPageItem;
+    });
+    
+    // Helper function to remove unwanted fields from a component
+    function sanitizeComponent(component) {
+      const sanitizedComponent = { ...component };
+    
+      // Remove 'createdBy' and other unwanted fields
+      delete sanitizedComponent.publishedAt;
+      delete sanitizedComponent.updatedAt;
+      delete sanitizedComponent.createdAt;
+    
+      
+    
+      return sanitizedComponent;
+    }
+      if (sanitizedPage) {
+        ctx.body = sanitizedPage;
       } else {
         ctx.status = 404;
         ctx.body = {
